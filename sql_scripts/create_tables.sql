69274770b178efee9bb19dcf09781b575ca2762a
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-08-05 15:21:00 +0100

    Create Tbales in SQL WIP

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -1,26 +1,38 @@
 CREATE TABLE IF NOT EXISTS artists (
-    id VARCHAR(20),
-    name VARCHAR(50),
-    followers INT,
-    popularity INT,
-    url VARCHAR(100)
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS tracks (
-    id VARCHAR(20),
-    name VARCHAR(50),
-    artist_id VARCHAR(20),
-    duration TIME
+    id VARCHAR(23) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    genre VARCHAR(50),
+    followers INT NOT NULL,
+    -- Spotify uses a popularity index 0-100
+    popularity SMALLINT NOT NULL,
+    url VARCHAR,
     PRIMARY KEY (id),
-    FOREIGN KEY (artists.id, artist_id)
+    CHECK (
+        popularity >= 0
+        AND popularity <= 100
+    )
 );
-
+CREATE TABLE IF NOT EXISTS tracks (
+    id VARCHAR(23) NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    artist_id VARCHAR(23) NOT NULL,
+    duration_ms SMALLINT NOT NULL,
+    popularity SMALLINT NOT NULL,
+    is_in_album BOOLEAN,
+    is_explicit BOOLEAN,
+    external_url VARCHAR,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_artist_id FOREIGN KEY (artist_id) REFERENCES artists(id),
+    CHECK (
+        popularity >= 0
+        AND popularity <= 100
+    )
+);
 CREATE TABLE IF NOT EXISTS listens (
-    track_id VARCHAR(20),
-    artist_id VARCHAR(20),
-    time_played DATETIME,
-    duration_playes TIME
-    FOREIGN KEY (tracks.id, tracks_id),
-    FOREIGN KEY (artists.id, artist_id)
+    track_id VARCHAR(23),
+    artist_id VARCHAR(23),
+    time_played TIMESTAMP,
+    play_duration_ms SMALLINT,
+    CONSTRAINT fk_track_id FOREIGN KEY(track_id) REFERENCES tracks(id),
+    CONSTRAINT fk_artist_id FOREIGN KEY(artist_id) REFERENCES artists(id)
 );

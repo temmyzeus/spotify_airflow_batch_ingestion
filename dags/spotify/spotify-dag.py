b17e4e83b981d538c3f82569378df4b7c1bcdf85
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-08-23 13:15:58 +0100

    Successfully  fetch data

--- dags/spotify/spotify-dag.py
+++ dags/spotify/spotify-dag.py
@@ -1,26 +1,23 @@
 import base64
-import json
 import os
-import time
 import urllib.parse
-from collections import defaultdict
+from collections import OrderedDict, defaultdict
 from datetime import datetime
 from typing import Any, Dict
 
 import airflow
-import boto3
 import pandas as pd
-import psycopg2
 import requests
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.utils.dates import days_ago
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 USERNAME: str = os.environ["SPOTIFY_USERNAME"]
 PASSWORD: str = os.environ["SPOTIFY_PASSWORD"]
@@ -35,11 +32,11 @@ OAUTH_ACCESS_TOKEN_URL: str = "https://accounts.spotify.com/api/token"
 CLIENT_KEY_B64: str = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode()).decode()
 
 
-def fetch_spotify_data() -> Any:
+def fetch_spotify_data(ti) -> Any:
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--window-size=1920x1080")
-    
+
     payload: dict[str, str] = {
         "client_id": CLIENT_ID,
         "response_type": "code",
@@ -55,7 +52,7 @@ def fetch_spotify_data() -> Any:
     driver = webdriver.Remote(
         command_executor=DRIVER_PATH,
         desired_capabilities=DesiredCapabilities.CHROME,
-        options=options
+        options=options,
     )
     driver.get(open_url)
     driver.find_element(By.ID, "login-username").send_keys(USERNAME)
@@ -64,11 +61,9 @@ def fetch_spotify_data() -> Any:
         By.XPATH,
         "/html/body/div[1]/div/div[2]/div/div/div[2]/div[3]/div[2]/button/div[1]/p",
     ).click()
-    
+
     try:
-        element = WebDriverWait(driver, timeout=240).until(
-            EC.title_is("Google")
-        )
+        element = WebDriverWait(driver, timeout=240).until(EC.title_is("Google"))
         current_url = driver.current_url
     finally:
         driver.quit()
@@ -103,31 +98,75 @@ def fetch_spotify_data() -> Any:
     )
     recently_played = recently_played_response.json()
 
-    listens = defaultdict(list)
-    artists = defaultdict(list)
+    listens:defaultdict = defaultdict(list)
+    artists: OrderedDict = OrderedDict(
+        {
+            "id": [],
+            "name": [],
+            "genre": [],
+            "followers": [],
+            "popularity": [],
+            "url": [],
+        }
+    )
+    tracks: OrderedDict = OrderedDict(
+        {
+            "id": [],
+            "name": [],
+            "artist_id": [],
+            "duration_ms": [],
+            "popularity": [],
+            "is_in_album": [],
+            "is_explicit": [],
+            "external_url": [],
+        }
+    )
 
-    print(recently_played.keys())
     for n, item in enumerate(recently_played["items"]):
         artist_id: str = item["track"]["artists"][0]["id"]
+        track_id: str = item["track"]["id"]
 
+        listens["track_id"].append(track_id)
         listens["artist_id"].append(artist_id)
-        listens["artists_name"].append(item["track"]["artists"][0]["name"])
-        listens["track_id"].append(item["track"]["id"])
-        listens["track_name"].append(item["track"]["name"])
         listens["time_played"].append(item["played_at"])
-        listens["duration_ms"].append(item["track"]["duration_ms"])
-        listens["is_explicit"].append(item["track"]["explicit"])
-        listens["is_album"].append(
-            True if (item["track"]["album"]["album_type"] == "album") else False
-        )
-        # next_url = recent_plays["next"]
-        # history["track_release_date"] = item["track"]["release_date"]
+        listens["play_duration_ms"].append(item["track"]["duration_ms"])
 
-        # artist_reponse = spotify.artist(artist_id)
+        artist_response = requests.get(
+            f"https://api.spotify.com/v1/artists/{artist_id}", headers=headers
+        )
+        artist = artist_response.json()
+
+        if artist_id not in artists["id"]:
+            artists["id"].append(artist_id)
+            artists["name"].append(artist["name"])
+            artists["genre"].append(artist["genres"])
+            artists["followers"].append(artist["followers"])
+            artists["popularity"].append(artist["popularity"])
+            artists["url"].append(artist["external_urls"]["spotify"])
+
+        track_response = requests.get(
+            f"https://api.spotify.com/v1/tracks/{track_id}", headers=headers
+        )
+        track = track_response.json()
+
+        if track_id not in tracks["id"]:
+            tracks["id"].append(track_id)
+            tracks["name"].append(track["name"])
+            tracks["artist_id"].append(artist_id)
+            tracks["duration_ms"].append(track["duration_ms"])
+            tracks["popularity"].append(track["popularity"])
+            tracks["is_in_album"].append(
+                True if track["album"]["album_type"] == "album" else False
+            )
+            tracks["is_explicit"].append(track["explicit"])
+            tracks["external_url"].append(track["external_urls"]["spotify"])
+
+    ti.xcom_push(key="artists", value=artists)
+    ti.xcom_push(key="tracks", value=tracks)
+    ti.xcom_push(key="listens", value=listens)
 
     listens_df = pd.DataFrame(listens)
     print(listens_df)
-    # How does return work in airflow?
 
 
 def upload_to_aws_rds():
